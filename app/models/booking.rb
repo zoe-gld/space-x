921ef85661ebe4574900d81c8d8fcf73c4ad7add
Zoe Goldschmidt <zoe.gld@gmail.com>
2019-11-18 17:23:33 +0000

    updated uniqueness scope for booking dates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :space
   belongs_to :user
   has_many :reviews
-  validates :date, presence: true, uniqueness: true
+  validates :date, presence: true, uniqueness: { scope: :space_id }
 end

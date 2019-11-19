31be2d2a2957c476e3c24622c2d2146385f2b86c
Zoe Goldschmidt <zoe.gld@gmail.com>
2019-11-19 11:14:58 +0000

    Views content and last space with cover image

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'spaces#home'
-  resources :spaces, except: :delete
+
+  resources :spaces
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

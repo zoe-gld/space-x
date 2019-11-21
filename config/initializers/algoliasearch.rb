fe6d58b38cd5bf1dbecb75cd0722ff26e2c23b0f
Oliver Allmoslechner <allmoslechner@gmail.com>
2019-11-21 13:27:30 +0000

    fixed some css and html issues in show page

---
+++ config/initializers/algoliasearch.rb
@@ -0,0 +1,9 @@
+AlgoliaSearch.configuration = {
+  application_id: 'N3OU7O8UZ5',
+  api_key: ALGOLIA_API_KEY,
+  connect_timeout: 2,
+  receive_timeout: 30,
+  send_timeout: 30,
+  batch_timeout: 120,
+  search_timeout: 5
+}

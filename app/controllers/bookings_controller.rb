97212e5dbe110954525322856e9ca0a517420c14
Oliver Allmoslechner <allmoslechner@gmail.com>
2019-11-22 16:15:53 +0000

    added ad of our friends from dodgehub

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,7 +7,7 @@ class BookingsController < ApplicationController
     @booking.space = @space
     @booking.user = current_user
     if @booking.save
-      redirect_to space_path(@space, anchor: 'bookings')
+      redirect_to space_path(@space, anchor: 'place-video'), flash: { exampleModalCenter: true }
     else
       @taken_dates = @space.bookings.map { |booking| booking.date }
       @review = Review.new
